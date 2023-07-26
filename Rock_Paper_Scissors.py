c9fa312032d4d4697edf74eafe4b5a22c0e97b48
musbahu ghassan <musbahughassan164@gmail.com>
2023-07-26 03:43:08 +0100

    upgrading

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -4,29 +4,28 @@ option = ["r", "p", "s"]
 user_wins = 0
 ai_wins = 0
 
-print("Type 'y' to play or 'q' to quit. ")
-
-while True:
-    user_input = input("r / p / s: ")
+started = input("TYPE 'Y' TO PLAY OR 'Q' TO EXIT: ").lower()
+while started == "y":
+    user_input = input("R-> P-> S:--->  ")
     if user_input == "q":
         break
     elif user_input not in option:
         continue
 
     ai_pick = random.choice(option)
-    print(f"A.I picked {ai_pick.upper()} and You picked {user_input.upper()}")
+    print(f"A.I PICKED--->{ai_pick.upper()}<---AND YOU PICKED--->{user_input.upper()}<---")
     if user_input == ai_pick:
         user_wins += 1
-        print("You Win")
+        print("--->You Win<---")
         
     elif user_input != ai_pick:
         ai_wins += 1
-        print("A.I Win")
-print("-------You scored: ", user_wins)
-print("-------A.I scored: ", ai_wins)
+        print("--->A.I Win<---")
+print("------->You scored: ", user_wins)
+print("------->A.I scored: ", ai_wins)
 if ai_wins > user_wins:
-    print("--------You are Eliminated.-------")
-    print("------------TryAgain--------------")
+    print("-------->You are Eliminated.<-------")
+    print("------------>TryAgain<--------------")
 elif user_wins > ai_wins:
-    print("--------You Won The Match.-------")
-    print("--------You are The Champion-----")
+    print("-------->You Won The Match.<-------")
+    print("-------->You are The Champion<-----")
